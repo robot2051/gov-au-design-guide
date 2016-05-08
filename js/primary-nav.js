2890466c673781647efd4d9c726559b3f1cf52b7
Catherine Jones <cath@bitgirl101.com>
2016-05-08 22:15:24 +1000

    refactor sass html js

--- js/primary-nav.js
+++ js/primary-nav.js
@@ -1,13 +1,13 @@
 $(window).on("load resize",function(e) {
   var more = document.getElementById("js-centered-more");
-  var menuToggle = $("#js-centered-navigation-mobile-menu").unbind();
-  $("#js-centered-navigation-menu").removeClass("show");
+  var menuToggle = $("#js-primary-navigation-mobile-menu").unbind();
+  $("#js-primary-navigation-menu").removeClass("show");
 
   menuToggle.on("click", function(e) {
     e.preventDefault();
-    $("#js-centered-navigation-menu").slideToggle(function(){
-      if($("#js-centered-navigation-menu").is(":hidden")) {
-        $("#js-centered-navigation-menu").removeAttr("style");
+    $("#js-primary-navigation-menu").slideToggle(function(){
+      if($("#js-primary-navigation-menu").is(":hidden")) {
+        $("#js-primary-navigation-menu").removeAttr("style");
       }
     });
   });
